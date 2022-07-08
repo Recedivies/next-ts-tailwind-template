6983151e4ca6c75b1c76b2ef3077a1fb3f3e6281
=recedivies <ahmadhiprananta@gmail.com>
2022-07-08 21:12:15 +0700

    feat: added tailwind configuration + global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,42 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const withOpacity =
+  (variable) =>
+  ({ opacityValue }) =>
+    opacityValue === undefined
+      ? `rgb(var(${variable}))`
+      : `rgb(var(${variable}) / ${opacityValue})`;
+
+const getColorShades = (shades, name = "primary") =>
+  shades.reduce(
+    (a, v) => ({ ...a, [v]: withOpacity(`--tw-clr-${name}-${v}`) }),
+    {},
+  );
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Inter", ...fontFamily.sans],
+      },
+      colors: {
+        // Customize it on globals.css :root
+        primary: getColorShades([
+          50, 100, 200, 300, 400, 500, 600, 700, 800, 900,
+        ]),
+        dark: "#111",
+        light: "#ddd",
+      },
+      strokeWidth: {
+        0.5: "0.5",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
